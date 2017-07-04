a983762133fa82f7c4264e9d445822d4ce606800
zian <fhqsuhpv@163.com>
2017-07-04 10:11:34 +0800

    添加路由控制
    验证没有登录之前的其它路由不可访问

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const address = resolve => require(['@/pages/Address'], resolve);
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
     routes: [{
             path: '/',
             name: '',
@@ -24,22 +24,57 @@ export default new Router({
         {
             path: '/card',
             name: 'card',
-            component: card
+            component: card,
+            meta: {
+                requireAuth: true // 添加该字段，表示进入这个路由是需要登录的
+            }
         },
         {
             path: '/giftlist',
             name: 'giftlist',
-            component: giftlist
+            component: giftlist,
+            meta: {
+                requireAuth: true // 添加该字段，表示进入这个路由是需要登录的
+            }
         },
         {
             path: '/gift',
             name: 'gift',
-            component: gift
+            component: gift,
+            meta: {
+                requireAuth: true // 添加该字段，表示进入这个路由是需要登录的
+            }
         },
         {
             path: '/address',
             name: 'address',
-            component: address
+            component: address,
+            meta: {
+                requireAuth: true // 添加该字段，表示进入这个路由是需要登录的
+            }
         }
     ]
 })
+
+
+// 在路由跳转前验证是否需要auth
+router.beforeEach((to, from, next) => {
+    // 回到页面顶部
+    window.scrollTo(0, 0);
+    if (to.matched.some(r => r.meta.requireAuth)) { // 判断该路由是否需要登录权限
+        if (window.localStorage.getItem('token')) {
+            next();
+        } else {
+            next({
+                path: '/login',
+                query: {
+                    redirect: to.fullPath // 将跳转的路由path作为参数，登录成功后跳转到该路由
+                }
+            });
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
