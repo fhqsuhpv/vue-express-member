177ea1e753cf2261cc5e3099b5b27fb686f6d0fd
zian <fhqsuhpv@163.com>
2017-07-28 21:40:43 +0800

    去掉跨域日志输出

--- service/controller/coc.js
+++ service/controller/coc.js
@@ -1,7 +1,6 @@
 var whitelist = ['http://localhost:8080', 'http://127.0.0.1:8080', 'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop', 'http://bjfinelycup.cn', 'http://www.bjfinelycup.cn']
 var corsOptions = {
     origin: function(origin, callback) {
-        console.log(origin);
         if (whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
