a26bc90e8cb197a0a48f3bb0a91b99595f768c72
zian <fhqsuhpv@163.com>
2017-08-15 06:49:34 +0800

    开启 mysql 连接池

--- service/db/db.js
+++ service/db/db.js
@@ -5,6 +5,6 @@ module.exports = {
         user: 'root',
         password: 'fhqsuhpv',
         database: 'member',
-        //useConnectionPooling: true
+        useConnectionPooling: true
     }
 }
