91612c1879efeedb164919986a491ada216ca591
zian <fhqsuhpv@163.com>
2017-08-26 12:35:28 +0800

    后端允许跨域

--- service/utils/coc.js
+++ service/utils/coc.js
@@ -1,4 +1,4 @@
-var whitelist = ['http://localhost:8080', 'http://127.0.0.1:8080', 'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop', 'http://bjfinelycup.cn', 'http://www.bjfinelycup.cn', 'http://localhost:9527']
+var whitelist = ['http://localhost:8080', 'http://127.0.0.1:8080', 'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop', 'http://bjfinelycup.cn', 'http://www.bjfinelycup.cn', 'http://localhost:9527', 'http://backend.bjfinelycup.cn']
 var corsOptions = {
     origin: function(origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
